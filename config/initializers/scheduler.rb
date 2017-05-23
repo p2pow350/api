389139e868b770c38818784c0836aba90c9b7f93
Administrator <administrator@MacBook-di-Administrator.local>
2017-05-23 10:23:22 +0200

    .

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,24 +4,22 @@ require 'rufus-scheduler'
 #scheduler = Rufus::Scheduler.new(:lockfile => ".rufus-scheduler.lock")
 scheduler = Rufus::Scheduler.new
 
-unless scheduler.down?
-	# every hour - Mon-Tue (10:00 to 19:00)
-	scheduler.cron '0 10-19/1 * * 1-5' do
-		# do stuff
-		system("rake check_hour_traffic_quality")
-	end
-	
-	# every morning
-	scheduler.cron '50 08 * * *' do
-		# do stuff
-		system("rake check_today_traffic_quality")
-	end
-
-	# every morning
-	scheduler.cron '00 09 * * *' do
-		# do stuff
-		system("rake check_yesterday_client_usage")
-	end	
-	
+# every hour - Mon-Tue (10:00 to 19:00)
+scheduler.cron '0 10-19/1 * * 1-5' do
+	# do stuff
+	system("rake check_hour_traffic_quality")
 end
 
+# every morning
+scheduler.cron '50 08 * * *' do
+	# do stuff
+	system("rake check_today_traffic_quality")
+end
+
+# every morning
+scheduler.cron '00 09 * * *' do
+	# do stuff
+	system("rake check_yesterday_client_usage")
+end	
+
+scheduler.join
